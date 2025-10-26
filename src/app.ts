5f81dd090b1cbe6f7c45d690073ae34ca37b856f
Mateo Correa Restrepo <mateo.correa926@pascualbravo.edu.co>
2025-10-26 18:10:17 -0500

    first commit

---
+++ src/app.ts
@@ -0,0 +1,13 @@
+import express from 'express';
+const app = express();
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('Welcome to the E-commerce Backend!');
+});
+
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
